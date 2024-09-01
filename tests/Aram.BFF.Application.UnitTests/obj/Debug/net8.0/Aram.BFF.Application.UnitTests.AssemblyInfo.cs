89da9d9b6970ae302922ad928794f32414677650
Artyom Yeprikyan <artyomyeprikyan@outlook.com>
2024-09-02 03:42:53 +0400

    Added_gitIgnore

--- tests/Aram.BFF.Application.UnitTests/obj/Debug/net8.0/Aram.BFF.Application.UnitTests.AssemblyInfo.cs
+++ tests/Aram.BFF.Application.UnitTests/obj/Debug/net8.0/Aram.BFF.Application.UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aram.BFF.Application.UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4cb51b3bd250084a8a1d584fe21397d402b8c7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aram.BFF.Application.UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aram.BFF.Application.UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
